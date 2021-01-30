37384c091db2cac2faa7b80010d278336b632a56
Spencer Vaughan <spencer.vaughan@gmail.com>
2021-01-30 13:42:52 -0500

    added 'magic' middleware to the server.js

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 const express = require("express");
 
+
 const PORT = process.env.PORT || 8080;
 
 const app = express();
+
+app.use(express.static("public"));
+
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+
 
 app.listen(PORT, function() {
     console.log("Server listening on: http://localhost:"+ PORT);
