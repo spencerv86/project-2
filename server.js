436d7522ed207a617dd541a6c771122baefc707f
Andre Diop <itsjustandre@gmail.com>
2021-01-30 22:01:19 -0500

    Added garment controller
    - Commented out code for testing

--- server.js
+++ server.js
@@ -1,17 +1,19 @@
 const express = require("express");
 const exphbs = require("express-handlebars");
-
 const PORT = process.env.PORT || 8080;
-
 const app = express();
+const garmentsController = require("./controllers/garmentsController")
 
 app.use(express.static("public"));
-
+app.use(garmentsController);
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
+
+
+
 
 app.listen(PORT, function () {
   console.log("Server listening on: http://localhost:" + PORT);
