fcc071e6f0ac59ed259d68756fa24174a25c12ad
Alsharif Nahas <eid_alshareef@hotmail.com>
2021-02-01 10:56:55 -0500

    fixed the database issue.

--- models/outfit.js
+++ models/outfit.js
@@ -12,10 +12,11 @@ module.exports = function (sequelize, DataTypes) {
         allowNull: false,
       },
     });
-    Outfit.hasOne(models.Garment, { as: "Hat", foreignKey: 'hat_id'});
-    Outfit.hasOne(models.Garment, { as: "Shirt", foreignKey: 'shirt_id'});
-    Outfit.hasOne(models.Garment, { as: "Pant", foreignKey: 'pant_id'});
-    Outfit.hasOne(models.Garment, { as: "Shoe", foreignKey: 'shoe_id'});
-    Outfit.hasOne(models.Garment, { as: "Outer", foreignKey: 'outer_id'});
+    Outfit.hasOne(models.Garment, { as: "Hat", foreignKey: 'hat_id' });
+    Outfit.hasOne(models.Garment, { as: "Shirt", foreignKey: 'shirt_id' });
+    Outfit.hasOne(models.Garment, { as: "Pant", foreignKey: 'pant_id' });
+    Outfit.hasOne(models.Garment, { as: "Shoe", foreignKey: 'shoe_id' });
+    Outfit.hasOne(models.Garment, { as: "Outer", foreignKey: 'outer_id' });
   };
+  return Outfit;
 };
--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const exphbs = require("express-handlebars");
 
 const PORT = process.env.PORT || 8080;
 const app = express();
+const db = require("./models");
 const garmentsController = require("./controllers/garmentsController");
 const outfitsController = require("./controllers/outfitsController");
 const closetsController = require("./controllers/closetsController");
@@ -14,6 +15,7 @@ app.use(outfitsController);
 app.use(closetsController);
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
@@ -26,8 +28,15 @@ app.use("/closet", closetsController);
 app.use("/outfits", outfitsController);
 
 
+db.sequelize
+  .sync()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server is running on http://localhost:${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 
-app.listen(PORT, function () {
-  console.log("Server listening on: http://localhost:" + PORT);
-});
