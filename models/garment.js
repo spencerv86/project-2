79eb54948688cc31886532435ff0c19c580a19f9
Spencer Vaughan <spencer.vaughan@gmail.com>
2021-02-01 16:21:27 -0500

    set default values to test post route

--- models/garment.js
+++ models/garment.js
@@ -2,31 +2,34 @@ module.exports = function (sequelize, DataTypes) {
   const Garment = sequelize.define("Garment", {
     type: {
       type: DataTypes.STRING,
-      allowNull: false,
+      allowNull: true,
+      defaultValue: "Shirt"
     },
     name: DataTypes.STRING,
     color: {
       type: DataTypes.STRING,
-      allowNull: false,
+      allowNull: true,
+      defaultValue: "Red"
     },
     size: {
       type: DataTypes.STRING,
-      allowNull: false,
+      allowNull: true,
+      defaultValue: "L"
     },
   });
 
-  Garment.associate = function (models) {
-    Garment.belongsTo(models.Closet, {
-      foreignKey: {
-        allowNull: false,
-      },
-    });
-    Garment.hasOne(models.Outfit, { as: "Hat", foreignKey: 'hat_id'});
-    Garment.hasOne(models.Outfit, { as: "Shirt", foreignKey: 'shirt_id'});
-    Garment.hasOne(models.Outfit, { as: "Pant", foreignKey: 'pant_id'});
-    Garment.hasOne(models.Outfit, { as: "Shoe", foreignKey: 'shoe_id'});
-    Garment.hasOne(models.Outfit, { as: "Outer", foreignKey: 'outer_id'});
-  };
+  // Garment.associate = function (models) {
+  //   Garment.belongsTo(models.Closet, {
+  //     foreignKey: {
+  //       allowNull: true,
+  //     },
+  //   });
+  //   Garment.hasOne(models.Outfit, { as: "Hat", foreignKey: 'hat_id'});
+  //   Garment.hasOne(models.Outfit, { as: "Shirt", foreignKey: 'shirt_id'});
+  //   Garment.hasOne(models.Outfit, { as: "Pant", foreignKey: 'pant_id'});
+  //   Garment.hasOne(models.Outfit, { as: "Shoe", foreignKey: 'shoe_id'});
+  //   Garment.hasOne(models.Outfit, { as: "Outer", foreignKey: 'outer_id'});
+  // };
 
   return Garment;
 };
