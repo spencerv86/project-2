d9a6de1b773133db0e3ad2e0735eb297a07da264
dlhardi <derricklhardison@gmail.com>
2021-02-01 13:22:44 -0500

    Added Jquery .val method to capture values of form inputs and store them in a new variable

--- public/assets/js/new-garment.js
+++ public/assets/js/new-garment.js
@@ -2,5 +2,14 @@ $(document).ready(function () {
   $("#new-garment-form").on("submit", function (event) {
     event.preventDefault();
     console.log("Submitted the form");
+
+    const newGarment = {
+      garmentType: $("#garmentType").val(),
+      garmentName: $("garmentName").val(),
+      garmentColor: $("garmentColor").val(),
+      garmentSize: $("garmentSize").val(),
+    };
+
+    console.log(newGarment)
   });
 });
