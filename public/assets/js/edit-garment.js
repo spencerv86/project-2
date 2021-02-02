fc164182896b87aaff53694087b776d8fcc1c8dc
Andre Diop <itsjustandre@gmail.com>
2021-02-01 22:50:16 -0500

    Update edit-garment.js
    - Added a put route to edit JS

---
+++ public/assets/js/edit-garment.js
@@ -0,0 +1,36 @@
+$(document).ready(function () {
+    $("#new-garment-form").on("submit", function (event) {
+      event.preventDefault();
+      console.log("Submitted the form");
+  
+      const newGarment = {
+        type: $("#garmentType").val(),
+        name: $("#garmentName").val(),
+        color: $("#garmentColor").val(),
+        size: $("#garmentSize").val(),
+      };
+  
+      console.log(newGarment);
+  
+      if (
+        !newGarment.type ||
+        !newGarment.name ||
+        !newGarment.color ||
+        !newGarment.size
+      ) {
+        alert("Please complete all fields!");
+      } else {
+        $.ajax({
+          url: "/api/garments/:id",
+          method: "PUT",
+          data: newGarment,
+        }).then((response) => {
+          console.log(response);
+          window.location.href = "/garments";  
+        });
+      }
+    });
+  });
+  
+  //
+  
