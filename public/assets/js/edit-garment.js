f4c9ec398c43d7cfeff04957fa14abadec3ca803
Andre Diop <itsjustandre@gmail.com>
2021-02-02 13:15:42 -0500

    Edit Garment functionality
    - Adding an id value to the button, I was able to pull the id off of the item that is being edited

--- public/assets/js/edit-garment.js
+++ public/assets/js/edit-garment.js
@@ -1,36 +1,32 @@
 $(document).ready(function () {
-    $("#update-garment-form").on("submit", function (event) {
-      event.preventDefault();
-      console.log("Submitted the form");
-  
-      const newGarment = {
-        type: $("#garmentType").val(),
-        name: $("#garmentName").val(),
-        color: $("#garmentColor").val(),
-        size: $("#garmentSize").val(),
-      };
-  
-      console.log(newGarment);
-  
-      if (
-        !newGarment.type ||
-        !newGarment.name ||
-        !newGarment.color ||
-        !newGarment.size
-      ) {
-        alert("Please complete all fields!");
-      } else {
-        $.ajax({
-          url: "/api/garments/:id",
-          method: "PUT",
-          data: newGarment,
-        }).then((response) => {
-          console.log(response);
-          window.location.href = "/garments";  
-        });
-      }
-    });
+  $("#update-garment-form").on("submit", function (event) {
+    event.preventDefault();
+    console.log("Submitted the form");
+    const id = $("#addGarment").data("id");
+    const newGarment = {
+      type: $("#garmentType").val(),
+      name: $("#garmentName").val(),
+      color: $("#garmentColor").val(),
+      size: $("#garmentSize").val(),
+    };
+    if (
+      !newGarment.type ||
+      !newGarment.name ||
+      !newGarment.color ||
+      !newGarment.size
+    ) {
+      alert("Please complete all fields!");
+    } else {
+      $.ajax({
+        url: "/api/garments/" + id,
+        method: "PUT",
+        data: newGarment,
+      }).then((response) => {
+        console.log(response);
+        window.location.href = "/garments";
+      });
+    }
   });
-  
-  //
-  
+});
+
+//
