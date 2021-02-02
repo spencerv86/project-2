8fffc924943f42162d949f1d8baf002de65039f3
Andre Diop <itsjustandre@gmail.com>
2021-02-02 12:46:37 -0500

    Update seeds.sql
    - added seeds

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+INSERT INTO promethium_db.Garments (type,name,color,size,createdAt,UpdatedAt)
+VALUES ("Jacket","Oree NYC Leather Jacket","black","xl",now(),now()),
+("Shirt","Lucky Denim Shirt","Blue","L",now(),now()),
+("Shoes","Dr. Feelgood Dunks","Green/Red","12",now(),now()),
+("Jacket","ICS Seoul Gaurdian Varsity","Blue/White","xl",now(),now()),
+("Pants","North Face Class V","Camo","M",now(),now()),
+("Hat","Carhartt Beanie","Grey","O/S",now(),now());
