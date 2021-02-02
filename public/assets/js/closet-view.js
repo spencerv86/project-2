ea3dae3aa0dadede5be05b1c110db5df40b9a89d
Spencer Vaughan <spencer.vaughan@gmail.com>
2021-02-02 14:01:28 -0500

    added localStorage calls to fill boxes on reload

--- public/assets/js/closet-view.js
+++ public/assets/js/closet-view.js
@@ -1,72 +1,76 @@
 $(document).ready(function () {
+  const tileBox = $(".garment-tile");
 
-
-    const tileBox = $(".garment-tile");
-    // const localStorageGarments = { ...localStorage };
-    // const value = Object.values(localStorageGarments);
-
-    // if (localStorageGarments.length != 0) {
-    //     value.forEach(garment => {
-    //         const box = $("<div class='tile is-child box garment'>").append(`<span>${garment}</span>`);
-    //         tileBox.append(box);
-    //     });
-    // }
-
-    // listening to the icons to show garments
-    const btn = $(".garment-view").on("click", function () {
-        const type = $(this).data("name");
-        console.log(type)
-
-        $.get("/garments/" + type, (data) => {
-            console.log("hello");
-            if (type == undefined) {
-                window.location.href = "/closets/"
-            } else {
-                window.location.href = "/garments/" + type
-            }
-
-        })
-    })
-
-    // listening to the add icon
-    $(".fa-plus").on("click", function () {
-        const id = $(this).data("id");
-        const name = $(this).data("name");
-
-        $.ajax({
-            url: "/api/garments/"+id,
-            method: "GET",
-        }).then((response) => {
-            console.log(response[0])
-            fillBox(response[0].type, response[0].name)
-        });
-
-    function fillBox(type, name) {
-        console.log(type);
-        console.log(name)
-        let garmentType = type
-        $(`#${garmentType}-box`).empty();
-        let garmentInfo = $("<h3>").attr("data-id", `${id}`).text(name);
-        $(`#${garmentType}-box`).append(garmentInfo);
+  function loadLocal(type) {
+    let response = JSON.parse(localStorage.getItem(type));
+    // console.log(response);
+    if (response !== null) {
+      let garmentType = response.type;
+    //   console.log(response.type);
+      $(`#${garmentType}-box`).empty();
+      let garmentInfo = $("<h3>").attr("data-id", `${response.id}`).text(response.name);
+      $(`#${garmentType}-box`).append(garmentInfo);
     }
+    return response;
+  }
+  loadLocal("Shirt");
+  loadLocal("Hats");
+  loadLocal("Pants");
+  loadLocal("Shoes");
+  loadLocal("Outerwear");
 
-    })
+  // listening to the icons to show garments
+  const btn = $(".garment-view").on("click", function () {
+    const type = $(this).data("name");
+    console.log(type);
 
-    /**
- * Listening to the delete icon.
- * Deleting the garment based on the id
- */
-    $(".fa-trash-alt").on("click", function () {
-        const id = $(this).data("id")
-        $.ajax("/api/garments/" + id, {
-            type: "DELETE"
-        }).then(
-            function () {
-                console.log("Garment deleted");
-                // Reload the page to get the updated list
-                location.reload();
-            }
-        );
-    })
+    $.get("/garments/" + type, (data) => {
+      console.log("hello");
+      if (type == undefined) {
+        window.location.href = "/closets/";
+      } else {
+        window.location.href = "/garments/" + type;
+      }
+    });
+  });
 
-})
+  // listening to the add icon
+  $(".fa-plus").on("click", function () {
+    const id = $(this).data("id");
+    const name = $(this).data("name");
+
+    $.ajax({
+      url: "/api/garments/" + id,
+      method: "GET",
+    }).then((response) => {
+      console.log(response[0]);
+      fillBox(response[0]);
+    });
+
+    function fillBox(response) {
+      console.log(response.type);
+      console.log(response.name);
+      let garmentType = response.type;
+      $(`#${garmentType}-box`).empty();
+      let garmentInfo = $("<h3>").attr("data-id", `${id}`).text(response.name);
+      $(`#${garmentType}-box`).append(garmentInfo);
+      // let savedItems = {"itemName":response.name, "itemID":response.id}
+      localStorage.setItem(garmentType, JSON.stringify(response));
+    }
+  });
+
+  /**
+   * Listening to the delete icon.
+   * Deleting the garment based on the id
+   */
+  $(".fa-trash-alt").on("click", function () {
+    const id = $(this).data("id");
+    $.ajax("/api/garments/" + id, {
+      type: "DELETE",
+    }).then(function () {
+      console.log("Garment deleted");
+      // Reload the page to get the updated list
+      location.reload();
+    });
+  });
+});
